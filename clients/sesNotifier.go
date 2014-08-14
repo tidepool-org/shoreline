923399b38578d9dd013576a491481ad89d477777
Jamie <jamie@tidepool.org>
2014-08-14 12:58:15 -0700

    change name and just extract the sea parts we need

---
+++ clients/sesNotifier.go
@@ -0,0 +1,113 @@
+package clients
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+const (
+	endpoint = "https://email.us-east-1.amazonaws.com"
+)
+
+type (
+	SesNotifier struct {
+		Config SesNotifierConfig
+	}
+	SesNotifierConfig struct {
+		from      string `json:"fromAddress"`
+		secretKey string `json:"secretKey"`
+		accessKey string `json:"accessKey"`
+	}
+)
+
+func NewSesNotifier(configSource string) *SesNotifier {
+
+	if jsonConfig, err := ioutil.ReadFile(configSource); err == nil {
+
+		config := configure(jsonConfig)
+
+		return &SesNotifier{
+			Config: config,
+		}
+	} else {
+		panic(err)
+	}
+
+}
+
+func configure(jsonConfig []byte) SesNotifierConfig {
+	var config SesNotifierConfig
+	if err := json.Unmarshal(jsonConfig, &config); err != nil {
+		log.Fatal(err)
+	}
+	return config
+}
+
+func (c *SesNotifier) Send(to []string, subject string, msg string) (string, error) {
+
+	data := make(url.Values)
+	data.Add("Action", "SendEmail")
+	data.Add("Source", c.Config.from)
+	data.Add("Destination.ToAddresses.member.1", strings.Join(to, ", "))
+	data.Add("Message.Subject.Data", subject)
+	data.Add("Message.Body.Text.Data", msg)
+	data.Add("AWSAccessKeyId", c.Config.accessKey)
+
+	return c.sesPost(data)
+}
+
+func (c *SesNotifier) authorizationHeader(date string) []string {
+	h := hmac.New(sha256.New, []uint8(c.Config.secretKey))
+	h.Write([]uint8(date))
+	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", c.Config.accessKey, signature)
+	return []string{auth}
+}
+
+func (c *SesNotifier) sesPost(data url.Values) (string, error) {
+	body := strings.NewReader(data.Encode())
+	req, err := http.NewRequest("POST", endpoint, body)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	now := time.Now().UTC()
+	// date format: "Tue, 25 May 2010 21:20:27 +0000"
+	date := now.Format("Mon, 02 Jan 2006 15:04:05 -0700")
+	req.Header.Set("Date", date)
+
+	h := hmac.New(sha256.New, []uint8(c.Config.secretKey))
+	h.Write([]uint8(date))
+	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", c.Config.accessKey, signature)
+	req.Header.Set("X-Amzn-Authorization", auth)
+
+	r, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("http error: %s", err)
+		return "", err
+	}
+
+	resultbody, _ := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+
+	if r.StatusCode != 200 {
+		log.Printf("error, status = %d", r.StatusCode)
+
+		log.Printf("error response: %s", resultbody)
+		return "", errors.New(fmt.Sprintf("error code %d. response: %s", r.StatusCode, resultbody))
+	}
+
+	return string(resultbody), nil
+}
