29ebb9a27e59745fd6e700a06be14c857ff3d18f
Lennart Goedhart <lennart@tidepool.org>
2019-07-31 10:53:25 +1000

    chore(dep): migrated from dep to Go 1.11 modules

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm -rf dist
 mkdir dist
-go build -o dist/shoreline shoreline.go
-go build -o dist/user-roles tools/user-roles.go
+go build -mod=vendor -o dist/shoreline shoreline.go
+go build -mod=vendor -o dist/user-roles tools/user-roles.go
 cp start.sh dist/
 cp env.sh dist/
