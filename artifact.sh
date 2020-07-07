e017d42a4e5e7ef1233d833c1d947182ddb79c42
Fabrice Sougey <33152621+BigFab@users.noreply.github.com>
2020-07-07 21:29:50 +0200

    Automatic generation of openapi documentation (#35)
    * Automatic generation of openapi documentation
    
    * Add execution flag to buildDoc.sh

--- artifact.sh
+++ artifact.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/add-golang-soup/artifact/artifact_go.sh'
+wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/dblp/artifact/artifact_go.sh'
 chmod +x artifact_go.sh
 
 . ./version.sh
---
+++ buildDoc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eux
+# Generate OpenAPI documentation
+GOPATH=${GOPATH:-~/go}
+echo "Using GOPATH: ${GOPATH}"
+
+if [ ! -x "$GOPATH/bin/swag" ]; then
+  echo "Getting swag..."
+  go get -u github.com/swaggo/swag/cmd/swag
+fi
+
+$GOPATH/bin/swag --version
+$GOPATH/bin/swag init --generalInfo shoreline.go --output docs
+
+# When tag is present, openapi doc is renamed before being deployed to S3
+if [ -n "${TRAVIS_TAG:-}" ]; then
+    APP="${TRAVIS_REPO_SLUG#*/}"
+    APP_TAG="${APP}-${TRAVIS_TAG/dblp./}"
+    mkdir docs/openapi
+    mv docs/swagger.json docs/openapi/${APP_TAG}-swagger.json
+fi
