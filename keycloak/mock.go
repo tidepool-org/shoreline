ac136db7ca2d3a2ef2d308b60152490e34f5f1a4
Todd Kazakov <todd.kazakov@gmail.com>
2020-08-11 13:10:35 +0300

    Update mock keycloak client

--- keycloak/mock.go
+++ keycloak/mock.go
@@ -3,10 +3,13 @@ package keycloak
 import (
 	"context"
 	"golang.org/x/oauth2"
+	"time"
 )
 
 type MockClient struct {
 }
+
+var _ Client = &MockClient{}
 
 func (m *MockClient) Login(ctx context.Context, username, password string) (*oauth2.Token, error) {
 	return &oauth2.Token{
@@ -17,4 +20,17 @@ func (m *MockClient) Login(ctx context.Context, username, password string) (*oau
 
 func (m *MockClient) RevokeToken(ctx context.Context, token *oauth2.Token) error {
 	return nil
+}
+
+func (m *MockClient) IntrospectToken(ctx context.Context, token *oauth2.Token) (*TokenIntrospectionResult, error) {
+	return &TokenIntrospectionResult{
+		Active:        true,
+		Subject:       "70dd70dd70",
+		EmailVerified: true,
+		ExpiresAt:     time.Now().Unix() + 600,
+	}, nil
+}
+
+func (m *MockClient) RefreshToken(ctx context.Context, token *oauth2.Token) (*oauth2.Token, error) {
+	return token, nil
 }
