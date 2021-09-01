e1bac2db8627d0bce385d842a6df2f26ebaf872f
Ydris Rebibane <ydris@diabeloop.fr>
2021-09-01 10:02:45 +0200

    Remove artifact.sh and travis.yml as the build process changed (#70)

--- artifact.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -ex
-
-wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/dblp/artifact/artifact_go.sh'
-chmod +x artifact_go.sh
-
-. ./version.sh
-./artifact_go.sh go
